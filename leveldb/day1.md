8233f05c0d5686677e0b936ae432cbb0c40a006e
wangchuanyi <wang_cyi@163.com>
2023-05-02 12:23:12 +0800

    update

--- leveldb/day1.md
+++ leveldb/day1.md
@@ -3,9 +3,10 @@
 
 践行do-book:精通LevelDB（一）:源码编译安装
 
-### Building
-- 作者 https://github.com/erpeng
+1. Building
 
+- 作者 https://github.com/erpeng
+~~~
 git clone --recurse-submodules https://github.com/watchpoints/leveldb.git
 
 git submodule update --init
@@ -15,6 +16,20 @@ Quick start:
 
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build .
+cmake -DCMAKE_BUILD_TYPE=Debug .. && cmake --build .~~
+
+make install # 安装头文件
+~~~
+
+2. 编译自己的CMakeLists.txt
+
+g++ -o ken_test ken_test.cpp -lleveldb -lpthread -I../leveldb-master/include/ -L../leveldb-master/build/ -std=c++11
+
+
+
+
+
+
 
 
 
--- leveldb/example/day1/day1.cpp
+++ leveldb/example/day1/day1.cpp
@@ -8,18 +8,22 @@ int main() {
   leveldb::DB* db;
   leveldb::Options options;
   options.create_if_missing = true;
-  leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
+  leveldb::Status status = leveldb::DB::Open(options, "./testdb", &db);
   assert(status.ok());
 
-  std::string key = "apple";
-  std::string value = "A";
+  std::string key = "name";
+  std::string value = "wangchuanyi";
   std::string get;
 
   leveldb::Status s = db->Put(leveldb::WriteOptions(), key, value);
   
   if (s.ok()) s = db->Get(leveldb::ReadOptions(), key, &get);
-  if (s.ok()) std::cout << "读取到的与(key=" << key << ")对应的(value=" << get << ")" << std::endl;
-  else std::cout << "读取失败!" << std::endl;
+  if (s.ok()) {
+	std::cout << "读取到的与(key=" << key << ")对应的(value=" << get << ")" << std::endl;
+  }
+  else {
+	std::cout << "读取失败!" << std::endl;
+  }
 
   delete db;
 
